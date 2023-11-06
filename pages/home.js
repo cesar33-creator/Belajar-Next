e2c8537953da2efd2d82204ef2b251561f110a8a
CESAR RAIS AKHTAR <cesarrais33@gmail.com>
2023-11-06 20:10:39 +0700

    change props

--- pages/home.js
+++ pages/home.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Navbar from './components/Navbar';
 import Image from 'next/image';
 
-const Home = () => {
+const Home = ({posts}) => {
   return (
     <>
       <Head>
@@ -12,8 +12,24 @@ const Home = () => {
       <Navbar />
       <Image src="/next.svg" alt="Logo Next JS" width={100} height={100} />
       <h1>Ini Halaman Home</h1>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+        </div>
+      ))}
     </>
   );
 };
 
 export default Home;
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await res.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
